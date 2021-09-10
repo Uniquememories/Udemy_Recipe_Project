f3a5b0b2a7f44ec22d1913453d6a521777b4078e
Kimberly Luke <kimberly_luke@cable.comcast.com>
2021-09-10 14:43:47 -0400

    Added recipie & shopping -list navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, Output } from "@angular/core";
+import { EventEmitter } from "@angular/core";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>();
 
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
